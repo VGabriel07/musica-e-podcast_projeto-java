6f70996191b68e4e060cdc82a0ac3f414c53cbc0
VGabriel <victorbianchini419@gmail.com>
2025-04-26 18:10:03 -0300

    Projeto musicas e podcast

--- src/musicasfavoritas/modelos/Musica.java
+++ src/musicasfavoritas/modelos/Musica.java
@@ -4,25 +4,12 @@ public class Musica extends Audio {
     private String album;
     private String artista;
     private String generoMusical;
-
-    public String getAlbum() {
-        return album;
-    }
-
     public void setAlbum(String album) {
         this.album = album;
     }
 
-    public String getArtista() {
-        return artista;
-    }
-
     public void setArtista(String artista) {
         this.artista = artista;
-    }
-
-    public String getGeneroMusical() {
-        return generoMusical;
     }
 
     public void setGeneroMusical(String generoMusical) {
